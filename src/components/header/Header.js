4305e140032e9c7c237ab627ec5e5936fbaf77bc
Renan Hilario <rvahilario@gmail.com>
2022-01-30 19:54:26 -0300

    add header comp

---
+++ src/components/header/Header.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { Button } from '../button/Button';
+
+function Header() {
+  const router = useRouter();
+  const handleLogout = () => router.push('/api/auth/logout');
+
+  return (
+    <Container>
+      <Button onClick={handleLogout}>Logout</Button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+{
+  /* <Box
+        styleSheet={{
+          width: '100%',
+          marginBottom: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box> */
+}
+
+export default Header;
