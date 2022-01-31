515a5b7b6fcd7446f27214523924d9fc85697654
Renan Hilario <rvahilario@gmail.com>
2022-01-31 03:07:18 -0300

    refact input comp

---
+++ src/components/inputText/InputText.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const InputText = styled.textarea`
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutrals._100};
+  background-color: ${({ theme }) => theme.colors.neutrals._700};
+  width: 100%;
+  height: 100%;
+  border: 2px solid transparent;
+  border-radius: 10px;
+  padding: 5px 10px;
+  transition: 0.5s ease;
+  resize: none;
+
+  & :focus {
+    outline: none;
+    border: 2px solid ${({ theme }) => theme.colors.primary._600};
+    background-color: ${({ theme }) => theme.colors.neutrals._800};
+  }
+
+  ::-webkit-scrollbar {
+    border-radius: 10px;
+    width: 15px;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    box-shadow: inset 0 0 4px 4px ${({ theme }) => theme.colors.primary._300};
+    border: solid 4px transparent;
+  }
+`;
+
+export default InputText;
