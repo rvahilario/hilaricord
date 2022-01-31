c4a81caf82af18fccbd3998963182bc706667d8f
Renan Hilario <rvahilario@gmail.com>
2022-01-30 22:31:02 -0300

    add nickname comp

---
+++ src/components/typograph/Nickname.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function Nickname(props) {
+  return <StyledFont>{props.children}</StyledFont>;
+}
+
+const StyledFont = styled.div`
+  color: ${({ theme }) => theme.colors.neutrals._50};
+  /* font-family: 'godOfWar'; */
+  font-size: 22px;
+`;
+
+export default Nickname;
