b836a38decf86037ce489cf85f0433defc54e243
Renan Hilario <rvahilario@gmail.com>
2022-01-30 22:30:48 -0300

    improve avatar comp

--- src/components/avatars/Avatar.js
+++ src/components/avatars/Avatar.js
@@ -1,21 +1,10 @@
 import styled from 'styled-components';
 
-function Avatar(props) {
-  return (
-    <Container>
-      <StyledImage src={props.username} />
-    </Container>
-  );
-}
-
-const Container = styled.div``;
-
-const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
+const Avatar = styled.img`
+  width: 30px;
+  height: 30px;
   border-radius: 1000px;
-  /* border-image: url('/frameGOW.png'); */
-  flex: 1;
+  margin-right: 10px;
 `;
 
 export default Avatar;
