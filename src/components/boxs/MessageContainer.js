ba685c95594ba705be95311550df98f330c984a7
Renan Hilario <rvahilario@gmail.com>
2022-01-31 03:05:30 -0300

    fix styles

--- src/components/boxs/MessageContainer.js
+++ src/components/boxs/MessageContainer.js
@@ -1,37 +1,18 @@
 import styled from 'styled-components';
 
 function MessageContainer(props) {
-  return (
-    <Container
-      style={{
-        filter: `blur(${props.blur}px)`,
-        WebkitFilter: `blur(${props.blur}px)`,
-      }}
-    >
-      {/* <Box
-        styleSheet={{
-          flexDirection: {
-            xs: 'column',
-            sm: 'row',
-          },
-        }}
-      > */}
-      {/* </Box> */}
-      {props.children}
-    </Container>
-  );
+  return <Container>{props.children}</Container>;
 }
 
 const Container = styled.div`
+  position: relative;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   flex: 1;
   height: 90%;
-  /* background-color: theme.colors.neutrals._600; */
   flex-direction: column;
   border-radius: 10px;
   padding: 16px;
-  /* overflow: scroll; */
 `;
 
 export default MessageContainer;
