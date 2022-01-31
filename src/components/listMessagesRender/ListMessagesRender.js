f5648c34bf5d8fd2c1c41b83a70acb9c5ee2893f
Renan Hilario <rvahilario@gmail.com>
2022-01-31 03:08:03 -0300

    add logic to render msg

--- src/components/listMessagesRender/ListMessagesRender.js
+++ src/components/listMessagesRender/ListMessagesRender.js
@@ -2,34 +2,37 @@ import styled from 'styled-components';
 
 import Avatar from '../avatars/Avatar';
 import Nickname from '../typograph/Nickname';
+import SelfContainer from '../boxs/SelfContainer';
+import SenderContainer from '../boxs/SenderContainer';
 import theme from '../../styles/themes/spartan';
 
 function ListMessagesRender(props) {
   return (
     <Container>
-      <ListContainer>
-        {props.listMessages.map((message) => {
-          return (
-            <MessageContainer key={message.id}>
-              <InfoContainer>
-                <Avatar src={message.picture} />
-                <Nickname>{message.sender}</Nickname>
-                <DateContainer>
-                  {new Date(message.created_at).toLocaleDateString()}
-                </DateContainer>
-              </InfoContainer>
-              {message.text_message}
-            </MessageContainer>
-          );
-        })}
-      </ListContainer>
+      {props.listMessages.map((message) => {
+        return props.user.nickname === message.sender ? (
+          <SelfContainer message={message} key={message.id}></SelfContainer>
+        ) : (
+          <SenderContainer message={message} key={message.id}></SenderContainer>
+        );
+      })}
     </Container>
   );
 }
 
+export default ListMessagesRender;
+
 const Container = styled.div`
-  height: 85%;
-  overflow-y: scroll;
+  /* max-height: 90%; */
+  /* height: 100%; */
+  /* background-color: red; */
+  margin-bottom: 5px;
+  display: flex;
+  flex: 1;
+  flex-direction: column-reverse;
+  overflow: auto;
+  color: ${theme.colors.neutrals._50};
+  margin-bottom: 16px;
 
   ::-webkit-scrollbar {
     border-radius: 10px;
@@ -42,37 +45,36 @@ const Container = styled.div`
   }
 `;
 
-const ListContainer = styled.ul`
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  color: ${theme.colors.neutrals._50};
-  margin-bottom: 16px;
-`;
+// const SenderContainer = styled.div`
+//   border-radius: 10px;
+//   padding: 6px;
+//   margin-bottom: 12px;
+//   transition: 0.3s;
+//   /* display: flex; */
+//   /* flex: 2; */
+//   /* flex-direction: row-reverse; */
 
-const MessageContainer = styled.li`
-  overflow: hidden;
-  border-radius: 10px;
-  padding: 6px;
-  margin-bottom: 12px;
-  transition: 0.3s;
+//   & :hover {
+//     background-color: ${theme.colors.primary._600};
+//   }
+// `;
 
-  & :hover {
-    background-color: ${theme.colors.primary._600};
-  }
-`;
+// const Text = styled.div`
+//   display: flex;
+//   margin-right: auto;
 
-const InfoContainer = styled.div`
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: row;
-`;
+//   flex-direction: row-reverse;
+// `;
 
-const DateContainer = styled.div`
-  margin-left: auto;
-  color: ${theme.colors.neutrals._300};
-  font-size: 12px;
-`;
+// const InfoContainer = styled.div`
+//   margin-bottom: 10px;
+//   display: flex;
+//   flex-direction: row-reverse;
+// `;
 
-export default ListMessagesRender;
+// const DateContainer = styled.div`
+//   /* margin-left: auto; */
+//   margin-right: auto;
+//   color: ${theme.colors.neutrals._300};
+//   font-size: 12px;
+// `;
