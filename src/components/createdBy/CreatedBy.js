7ce5fd86f1e5a8438ba7aaf823cb0c3cc260c843
Renan Hilario <rvahilario@gmail.com>
2022-02-02 03:58:28 -0300

    improvement styles

--- src/components/createdBy/CreatedBy.js
+++ src/components/createdBy/CreatedBy.js
@@ -2,59 +2,59 @@ import styled from 'styled-components';
 import { FaLinkedin, FaGithubSquare } from 'react-icons/fa';
 
 function CreatedBy() {
-  return (
-    <Container>
-      <h2>
-        Developed by <br />
-        Renan Hilário
-      </h2>
-      <li>
-        <a
-          href={'https://www.linkedin.com/in/renan-hilario/'}
-          target={'_blank'}
-        >
-          <div>
-            <FaLinkedin />
-          </div>
-        </a>
-        <a href={'https://github.com/rvahilario'} target={'_blank'}>
-          <div>
-            <FaGithubSquare />
-          </div>
-        </a>
-      </li>
-    </Container>
-  );
+	return (
+		<Container>
+			<h2>
+				Developed by <br />
+				Renan Hilário
+			</h2>
+			<li>
+				<a
+					href={'https://www.linkedin.com/in/renan-hilario/'}
+					target={'_blank'}
+				>
+					<div>
+						<FaLinkedin />
+					</div>
+				</a>
+				<a href={'https://github.com/rvahilario'} target={'_blank'}>
+					<div>
+						<FaGithubSquare />
+					</div>
+				</a>
+			</li>
+		</Container>
+	);
 }
 
 export default CreatedBy;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: ${({ theme }) => theme.colors.neutrals._200};
-  margin-left: 3%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	color: ${({ theme }) => theme.colors.neutrals._200};
+	margin-left: 3%;
 
-  h2 {
-    font-size: 0.8rem;
-  }
+	h2 {
+		font-size: 0.8rem;
+	}
 
-  li {
-    margin-top: 0.5rem;
-    display: flex;
-    flex-direction: row;
+	li {
+		margin-top: 0.5rem;
+		display: flex;
+		flex-direction: row;
 
-    a {
-      color: ${({ theme }) => theme.colors.primary._200};
-      margin: 0.5rem;
-      font-size: 2rem;
-      transition: 0.3s;
+		a {
+			color: ${({ theme }) => theme.colors.neutrals._200};
+			margin: 0.5rem;
+			font-size: 2rem;
+			transition: 0.3s;
 
-      :hover {
-        color: ${({ theme }) => theme.colors.primary._900};
-      }
-    }
-  }
+			:hover {
+				color: ${({ theme }) => theme.colors.primary._700};
+			}
+		}
+	}
 `;
