771070fc02b638780099aa66f40b29b795efc809
Renan Hilario <rvahilario@gmail.com>
2022-02-03 00:31:11 -0300

    fix font-size to huge nicknames

--- src/components/typograph/Nickname.js
+++ src/components/typograph/Nickname.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 function Nickname(props) {
-  return <StyledFont>{props.children}</StyledFont>;
+	return <StyledFont>{props.children}</StyledFont>;
 }
 
 const StyledFont = styled.div`
-  color: ${({ theme }) => theme.colors.neutrals._100};
-  font-size: 1.2rem;
-  margin-right: 10px;
+	color: ${({ theme }) => theme.colors.neutrals._100};
+	font-size: 1rem;
+	margin-right: 10px;
 `;
 
 export default Nickname;
