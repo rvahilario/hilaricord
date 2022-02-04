6aaebcd61595411106c1da67a39cebde58a0b984
Renan Hilario <rvahilario@gmail.com>
2022-02-03 23:50:12 -0300

    styles improvements

--- src/components/background/BackgroundStatic.js
+++ src/components/background/BackgroundStatic.js
@@ -1,42 +1,42 @@
 import styled from 'styled-components';
 
 const BackgroundStatic = (props) => {
-  const imageBackground = '/kratos_atreus.jpg';
+	const imageBackground = '/kratos_atreus.jpg';
 
-  return (
-    <ImageContainer>
-      <img
-        className="image"
-        src={imageBackground}
-        style={{
-          filter: `blur(${props.blur}px)`,
-          WebkitFilter: `blur(${props.blur}px)`,
-        }}
-      />
-      {props.children}
-    </ImageContainer>
-  );
+	return (
+		<ImageContainer>
+			<img
+				className="image"
+				src={imageBackground}
+				style={{
+					filter: `blur(${props.blur}px)`,
+					WebkitFilter: `blur(${props.blur}px)`,
+				}}
+			/>
+			{props.children}
+		</ImageContainer>
+	);
 };
 
 const ImageContainer = styled.div`
-  height: 100vh;
-  min-width: 220px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.4);
+	height: 102vh;
+	min-width: 220px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.4);
 
-  & .image {
-    object-fit: cover;
-    width: 100%;
-    height: 100vh;
-    min-width: 220px;
-    object-position: 40%;
-    position: fixed;
-    z-index: -10;
-    overflow: hidden;
-  }
+	& .image {
+		object-fit: cover;
+		width: 100%;
+		height: 102vh;
+		min-width: 220px;
+		object-position: 40%;
+		position: fixed;
+		z-index: -10;
+		overflow: hidden;
+	}
 `;
 
 export default BackgroundStatic;
--- src/components/background/BackgroundVideo.js
+++ src/components/background/BackgroundVideo.js
@@ -1,45 +1,45 @@
 import styled from 'styled-components';
 
 const BackgroundVideo = (props) => {
-  const videoSource = '/bg_video.mp4';
-  return (
-    <VideoContainer>
-      <video
-        className="video"
-        src={videoSource}
-        autoPlay
-        loop
-        muted
-        style={{
-          filter: `blur(${props.blur}px)`,
-          WebkitFilter: `blur(${props.blur}px)`,
-        }}
-      />
-      {props.children}
-    </VideoContainer>
-  );
+	const videoSource = '/bg_video.mp4';
+	return (
+		<VideoContainer>
+			<video
+				className="video"
+				src={videoSource}
+				autoPlay
+				loop
+				muted
+				style={{
+					filter: `blur(${props.blur}px)`,
+					WebkitFilter: `blur(${props.blur}px)`,
+				}}
+			/>
+			{props.children}
+		</VideoContainer>
+	);
 };
 
 const VideoContainer = styled.div`
-  height: 100vh;
-  min-width: 220px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.4);
+	height: 102vh;
+	min-width: 220px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.4);
 
-  & .video {
-    object-fit: cover;
-    width: 100%;
-    height: 100vh;
-    min-width: 220px;
-    position: fixed;
-    z-index: -10;
-    overflow: hidden;
-    object-position: 80% 50%;
-  }
+	& .video {
+		object-fit: cover;
+		width: 100%;
+		height: 102vh;
+		min-width: 220px;
+		position: fixed;
+		z-index: -10;
+		overflow: hidden;
+		object-position: 80% 50%;
+	}
 `;
 
 export default BackgroundVideo;
